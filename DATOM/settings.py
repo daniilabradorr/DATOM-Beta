00f2ecfd4b0289bc96fc7dcbae94fdf656f9775f
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-03-31 22:25:37 +0200

    modificando settgoins 4

--- DATOM/settings.py
+++ DATOM/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
